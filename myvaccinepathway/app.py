60d8c7024785d04f189c8a566706310c9a99e636
Ocean <haghighidaly@gmail.com>
2021-11-09 22:35:23 +0000

    Working example with linear correlations for 2 hardcoded vaccines

--- myvaccinepathway/app.py
+++ myvaccinepathway/app.py
@@ -1,2 +1,154 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+from datetime import date, timedelta
+from typing import List
 
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+INITIAL_IMMUNITY_LEVEL = 0
+
+PFIZER = "pfizer"
+AZ = "astrazeneca"
+MODERNA = "moderna"
+
+# User input
+vaccine = st.selectbox("Vaccine", options=[PFIZER, AZ, MODERNA])
+dose_1 = st.date_input("Dose 1", value=date.today() - timedelta(120))
+dose_2 = st.date_input("Dose 2", value=date.today() - timedelta(30))
+end_date = st.date_input("End date", value=date.today())
+
+start_date = dose_1 - timedelta(10)
+
+DOSE_1_PEAK_IMMUNITY_LEVEL = {
+    PFIZER: 0.3,
+    AZ: 0.33,  # Dummy value
+    MODERNA: 0.3,  # Dummy value
+}
+DOSE_2_PEAK_IMMUNITY_LEVEL = {
+    PFIZER: 0.95,
+    AZ: 0.7,  # Dummy value
+    MODERNA: 0.95,  # Dummy value
+}
+DOSE_PEAK_IMMUNITY_LEVEL = [DOSE_1_PEAK_IMMUNITY_LEVEL, DOSE_2_PEAK_IMMUNITY_LEVEL]
+
+
+DOSE_1_PEAK_IMMUNITY_DAY = {
+    PFIZER: 12,
+    AZ: 12,  # Dummy value
+    MODERNA: 12,  # Dummy value
+}
+DOSE_2_PEAK_IMMUNITY_DAY = {
+    PFIZER: 7,
+    AZ: 7,  # Dummy value
+    MODERNA: 7,  # Dummy value
+}
+DOSE_PEAK_IMMUNITY_DAY = [DOSE_1_PEAK_IMMUNITY_DAY, DOSE_2_PEAK_IMMUNITY_DAY]
+
+
+DOSE_1_IMMUNITY_WANING_RATE = {
+    PFIZER: -0.001,  # Dummy value
+    AZ: -0.001,  # Dummy value
+    MODERNA: -0.001,  # Dummy value
+}
+DOSE_2_IMMUNITY_WANING_RATE = {
+    PFIZER: -0.001,  # Dummy value
+    AZ: -0.001,  # Dummy value
+    MODERNA: -0.001,  # Dummy value
+}
+DOSE_IMMUNITY_WANING_RATE = [DOSE_1_IMMUNITY_WANING_RATE, DOSE_2_IMMUNITY_WANING_RATE]
+
+
+# def get_immunity_level(vaccine=PFIZER):
+#     # TODO: Logic
+#     immunity_level_by_day = []
+#     if dose_1:
+#         # Dose 1
+#         immunity_level_by_day += [INITIAL_IMMUNITY_LEVEL * i for i in range(0, dose_1)]
+#         immunity_level_by_day += [INITIAL_IMMUNITY_LEVEL + DOSE_1_INCREASING_IMMUNITY_RATE[vaccine]*i for i in range(DOSE_1_PEAK_IMMUNITY_DAY[vaccine])]
+#         if not dose_2:
+#             immunity_level_by_day += [DOSE_1_PEAK_IMMUNITY_LEVEL[vaccine] - DOSE_1_IMMUNITY_WANING_RATE[vaccine] * i for i in range(end_day - DOSE_1_PEAK_IMMUNITY_DAY[vaccine] - dose_1 + 1)]
+#
+#         # Dose 2
+#         immunity_level_by_day += [DOSE_1_PEAK_IMMUNITY_LEVEL[vaccine] - DOSE_1_IMMUNITY_WANING_RATE[vaccine] * i for i in range(dose_2 - DOSE_1_PEAK_IMMUNITY_DAY[vaccine] - dose_1 + 1)]
+#
+#     return immunity_level_by_day
+
+
+def _convert_dates_to_relative_days(start_date, end_date, dose_dates):
+    start_day = 0
+    end_day = (end_date - start_date).days
+    dose_days = [(dose_date - start_date).days for dose_date in dose_dates]
+    return start_day, end_day, dose_days
+
+
+def linear_plot(start_day, end_day, start_value, end_value):
+    """ Inclusive of start day, exclusive of end day """
+    day_range = end_day - start_day
+    value_change = end_value - start_value
+    gradient = value_change/day_range
+    linear_values = [start_value + gradient*step for step in range(day_range)]
+    return linear_values
+
+
+def linear_plot_rate(start_day, end_day, start_value, rate, min_value=0):
+    """ Inclusive of start day, exclusive of end day """
+    day_range = end_day - start_day
+    linear_values = [start_value + rate*step for step in range(day_range)]
+    linear_values = [val if val > min_value else min_value for val in linear_values]
+    return linear_values
+
+
+def _calculate_immunity_levels(vaccine, start_day, end_day, dose_days):
+    immunity_levels = []
+
+    # If no doses given
+    if not dose_days:
+        return linear_plot(start_day, end_day, INITIAL_IMMUNITY_LEVEL, INITIAL_IMMUNITY_LEVEL)
+
+    immunity_levels += linear_plot(start_day, dose_days[0], INITIAL_IMMUNITY_LEVEL, INITIAL_IMMUNITY_LEVEL)
+    immunity_levels += linear_plot(dose_days[0],
+                                   dose_days[0] + DOSE_1_PEAK_IMMUNITY_DAY[vaccine],
+                                   INITIAL_IMMUNITY_LEVEL,
+                                   DOSE_1_PEAK_IMMUNITY_LEVEL[vaccine])
+    immunity_levels += linear_plot_rate(dose_days[0] + DOSE_1_PEAK_IMMUNITY_DAY[vaccine],
+                                   dose_days[1],
+                                   DOSE_1_PEAK_IMMUNITY_LEVEL[vaccine],
+                                   DOSE_1_IMMUNITY_WANING_RATE[vaccine])
+    immunity_levels += linear_plot(dose_days[1],
+                                   dose_days[1] + DOSE_2_PEAK_IMMUNITY_DAY[vaccine],
+                                   immunity_levels[-1],
+                                   DOSE_2_PEAK_IMMUNITY_LEVEL[vaccine])
+    immunity_levels += linear_plot_rate(dose_days[1] + DOSE_2_PEAK_IMMUNITY_DAY[vaccine],
+                                        end_day,
+                                        DOSE_2_PEAK_IMMUNITY_LEVEL[vaccine],
+                                        DOSE_2_IMMUNITY_WANING_RATE[vaccine])
+
+    return immunity_levels
+
+
+def get_immunity_level(vaccine: str, start_date: date, end_date: date=date.today(), dose_dates: List[date] = None):
+    if not dose_dates:
+        dose_dates = []
+    assert start_date < end_date, "Start date cannot be after end date"
+    assert all([(start_date < dose_date) and (dose_date < end_date) for dose_date in dose_dates]), "Dose dates must be between start and end dates"
+
+    start_day, end_day, dose_days = _convert_dates_to_relative_days(start_date, end_date, dose_dates)
+    dates = list(daterange(start_date, end_date))
+    immunity_level = _calculate_immunity_levels(vaccine, start_day, end_day, dose_days)
+
+    data = {
+        "dates": dates,
+        "immunity_level": immunity_level
+    }
+
+    return data
+
+
+data = get_immunity_level(vaccine, start_date, end_date, [dose_1, dose_2])
+df = pd.DataFrame(data).set_index("dates", drop=True)
+
+st.line_chart(df)
