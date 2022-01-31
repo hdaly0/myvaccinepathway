a1f1d9f6fcd98db01bb2652959cc278374ae9809
Ocean <haghighidaly@gmail.com>
2022-01-31 01:51:55 +0000

    Update html information to correspond with new data

--- myvaccinepathway/html_snippets.py
+++ myvaccinepathway/html_snippets.py
@@ -15,6 +15,9 @@ HEAD_TITLE = """
     <br>
     <br>
     <h3 style="display:inline;">What is my current covid immunity?</h3>
+    <br>
+    <br>
+    <h5 style="display:inline; color: green">Updated 31 Jan. 2022 - Latest Omicron immunity data</h3>
 </div>
 <hr>
 """
@@ -41,7 +44,7 @@ ASSUMPTIONS_DELTA_DATA = """
 <h4 style="text-align: center;">Assumptions: Delta variant data</h4>
 <ul>
   <li>Doses 1 and 2 were of the same type. I.e. either both doses 1 and 2 were AstraZeneca, Pfizer, or Moderna, but not a combination of the three.</li>
-  <li>Doses 3 and onwards are assumed to be Pfizer jabs since these are generally the boosters and 3rd jabs on offer in the UK.</li>
+  <li>Booster/dose 3 vaccine data currently presented is a copy of dose 2 levels as this is a reasonable estimate until the real booster data can be inserted.</li>
   <li>Moderna data is less available than Astrazeneca and Pfizer due to it's later approval in the UK. Currently, Moderna data for hospitalisation and deaths isn't included. Where Moderna data is missing, the corresponding Pfizer data is used instead since the vaccine types are most similar and the trend seems to be Moderna having greater immunity levels, so using Pfizer will provide a lower bound.</li>
 </ul>
 <hr>
@@ -50,12 +53,8 @@ ASSUMPTIONS_DELTA_DATA = """
 REFERENCE_OMICRON_DATA = """
 <h4 style="text-align: center;">References: Omicron variant data</h4>
 <p>
-    Data is only available for primary doses of Astrazeneca and Pfizer, coupled with a Pfizer booster. The data presented
-    is also only a <b>model</b> of expected behaviour from the limited omicron data available and previous covid behaviours seen. The data
-    will be updated with real-world data as and when it becomes available.
-</p>
-<p>
-    The data presented is from <a href="https://www.imperial.ac.uk/mrc-global-infectious-disease-analysis/covid-19/report-48-global-omicron/" target="_blank">this paper by Imperial College London</a>.
+    Omicron data presented is from <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1050721/Vaccine-surveillance-report-week-4.pdf"> this survey by the UKHSA</a>.
+    The survey is an aggregation of several international efforts to analyse the limited omicron data becoming available, including data from the UK.
 </p>
 """
 
@@ -63,31 +62,24 @@ ASSUMPTIONS_OMICRON_DATA = """
 <hr>
 <h4 style="text-align: center;">Assumptions: Omicron variant data</h4>
 <ul>
-  <li>Dose 1 immunity levels were assumed to be 1/2 dose 2 immunity levels. Dose 1 immunity data was unavailable.</li>
-  <li>Some dose 2 data, and some booster jab data, was extrapolated based on patterns in the data that were available. For full details see comments in the code on <a href="https://github.com/hdaly0/myvaccinepathway" target="_blank">github</a>.</li>
-  <li>Dose 3 onwards are assumed to be Pfizer. Moderna jabs (primary and booster) are likely to have similar, if not higher, immunity values to Pfizer, so the Pfizer data provides a realistic lower bound for Moderna vaccinated individuals</li>
-  <li>Moderna data for the Omincron variant is not currently published so Pfizer data is used in its place. As above, this should be a reasonable lower bound for Moderna vaccinated individuals.</li>
+  <li>Dose 1 immunity data is unavailable so Dose 1 immunity levels were assumed to be 1/2 dose 2 immunity levels. This value was chosen as it was the assumption made in the <a href="https://www.imperial.ac.uk/mrc-global-infectious-disease-analysis/covid-19/report-48-global-omicron/">Imperial College London/WHO paper</a> that modelled Omicron immunity.</li>
+  <li>Fatality from Covid-19 data for Omicron Covid-19 is only currently available as an average value for Astrazeneca, Pfizer, and Moderna vaccinated individuals, so this average value is presented as the hospitalisation value of all three vaccine types.</li>
+  <li>Fatality from Covid-19 data for Omicron Covid-19 is currently extremely limited, with only 2 datapoints at 3 months post dose 2, and 2 weeks post dose 3. There is not currently data on immunity from fatal Covid-19 shortly after dose 2, or long after dose 3, which is why the fatality graphs do no show a decrease in immunity with time at the moment.</li>
+  <li>There is no Moderna immunity data for hospitalisation from Covid-19 so Pfizer data is used in its place. Moderna jabs are likely to have similar, if not higher, immunity values to Pfizer, so the Pfizer data provides a realistic lower bound for Moderna vaccinated individuals.</li>
+  <li>There is no AstraZeneca immunity data for hospitalisation from Covid-19 before 154 days after dose 2, so the values at 154 days are used as a lower bound for earlier dates. This is the reason the AstraZeneca hospitalisation from Covid-19 graph does not show a decrease in immunity after dose 2 at the moment.</li>
 </ul>
 <hr>
 """
 
 OMICRON_DATA_WARNING = """
 <p style="color: orange; text-align: center;">
-    <b>Note:</b> Omicron data is still limited since the variant has not been around long, so the full effects are still unknown.
-    The data presented here is from a model that uses some of the real-world data available. Newer data
-    coming to light (see <a href="https://www.imperial.ac.uk/mrc-global-infectious-disease-analysis/covid-19/report-50-severity-omicron/" target="_blank">this study</a> for example) 
-    is hinting towards the Omicron variant causing less severe illness, however this is still not confirmed.
+    <b>Note:</b> Some Omicron data is still limited, especially for immunity from fatal Covid-19 and some vaccine types - see Assumptions.
 </p>
 """
 
-MODERNA_OMICRON_DATA_WARNING = """
-<p style="color: orange; text-align: center;">Warning: Moderna data is limited so Pfizer data has been used in its place here. See Assumptions</p>
-"""
-
 MODERNA_DELTA_DATA_WARNING = """
-<p style="color: orange; text-align: center;">Warning: Moderna data for hospitalisation and death is limited so Pfizer data has been used in its place here. See Assumptions</p>
+<p style="color: orange; text-align: center;">Warning: Moderna data for hospitalisation and death has not yet been extracted so Pfizer data has been used in its place here. See Assumptions</p>
 """
-
 
 WHAT_IMMUNITY_LEVEL_MEANS = """
 <hr>
@@ -119,6 +111,13 @@ FAQ = """
     present the uncertainty present due to multiple factors. 
     For more information see <a href="https://www.who.int/news-room/feature-stories/detail/vaccine-efficacy-effectiveness-and-protection" target="_blank">this World Health Organisation article</a>.
 </p>
+<h6>Why is there a large range of possible values for some Omicron immunity values?</h6>
+<p>
+    Data for immunity from the Omicron variant of Covid-19 is still coming to light. When there is less data available
+    it makes the results of that data more uncertain, which is another way of saying the ranges of possible values
+    are larger. As more data becomes available we can expect the ranges of the values to decrease as we better
+    understand different immune reactions to Omicron.
+</p>
 <hr>
 """
 
@@ -129,11 +128,9 @@ ROADMAP = """
     for future features, but the following are on our list to implement:
 </p>
 <ul>
+  <li>Immunity provided by prior covid infection</li>
   <li>Age-based immunity data</li>
   <li>Immunity data for immunocompromised individuals</li>
-  <li>Immunity provided by prior covid infection</li>
-  <li>Further Omicron data when it becomes available</li>
-  <li>More detailed Moderna vaccine data when it becomes available</li>
   <li>Data for more vaccine types</li>
 </ul>
 <hr>
