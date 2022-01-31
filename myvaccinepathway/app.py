eedc907e28fd45ed0d28aaa299a31260b22576e5
Ocean <haghighidaly@gmail.com>
2022-01-31 01:52:20 +0000

    Improve wording of text

--- myvaccinepathway/app.py
+++ myvaccinepathway/app.py
@@ -8,7 +8,7 @@ from constants import DEFAULT_JAB_DATE, ALLOWED_PRIMARY_VACCINE_TYPES, ALLOWED_I
 from computation_functions import get_immunity, create_doses, get_start_and_end_dates
 from plotting_functions import get_plotly_timeline, get_plotly_figure, get_plotly_figure_error_bars
 from html_snippets import HEAD_TITLE, ASSUMPTIONS_DELTA_DATA, DISCLAIMER, PRODUCT_STAGES, \
-    ASSUMPTIONS_OMICRON_DATA, MODERNA_OMICRON_DATA_WARNING, MODERNA_DELTA_DATA_WARNING, WHAT_IMMUNITY_LEVEL_MEANS, \
+    ASSUMPTIONS_OMICRON_DATA, MODERNA_DELTA_DATA_WARNING, WHAT_IMMUNITY_LEVEL_MEANS, \
     FAQ, ROADMAP, CURRENT_IMMUNITY_TEXT_LAYOUT_3, \
     ABOUT_US, REFERENCE_DELTA_DATA, REFERENCE_OMICRON_DATA, OMICRON_DATA_WARNING
 
@@ -26,7 +26,7 @@ number_of_doses = st_centre.number_input("How many covid jabs have you had?", va
 
 # Use form with submit button so page doesn't recalculate every time, only on submit
 with st_centre.form(key='user_info_form'):
-    primary_vaccine_type = st.radio("Primary vaccine type", options=ALLOWED_PRIMARY_VACCINE_TYPES)
+    primary_vaccine_type = st.radio("Dose 1 & 2 vaccine type", options=ALLOWED_PRIMARY_VACCINE_TYPES)
     secondary_vaccine_type = None
     if number_of_doses > 2:
         secondary_vaccine_type = st.radio("Booster/dose 3 vaccine type", options=ALLOWED_SECONDARY_VACCINE_TYPES)
@@ -47,10 +47,6 @@ with st_centre.form(key='user_info_form'):
 # Only load the plots after submit button has been clicked
 if submit_button:
     # Present any data warnings for incomplete data
-    # Moderna
-    if (primary_vaccine_type == MODERNA) and (variant_type == OMICRON):
-        st_centre.markdown(MODERNA_OMICRON_DATA_WARNING, unsafe_allow_html=True)
-
     if (primary_vaccine_type == MODERNA) and (variant_type == DELTA):
         st_centre.markdown(MODERNA_DELTA_DATA_WARNING, unsafe_allow_html=True)
 
@@ -101,15 +97,15 @@ if submit_button:
     st_centre.plotly_chart(fig_timeline, use_container_width=True)
 
     # Symptomatic immunity plot
-    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[SYMPTOMATIC], "Symptomatic infection")
+    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[SYMPTOMATIC], "Immunity from Symptomatic Covid-19")
     st_centre.plotly_chart(fig_symptomatic_immunity, use_container_width=True)
 
     # Hospitalisation plot
-    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[HOSPITALISATION], "Hospitalisation")
+    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[HOSPITALISATION], "Immunity from Covid-19 Hospitalisation")
     st_centre.plotly_chart(fig_symptomatic_immunity, use_container_width=True)
 
     # Death plot
-    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[DEATH], "Death")
+    fig_symptomatic_immunity = get_plotly_figure(immunity_dfs[DEATH], "Immunity from Fatal Covid-19")
     st_centre.plotly_chart(fig_symptomatic_immunity, use_container_width=True)
 
     # Assumptions
